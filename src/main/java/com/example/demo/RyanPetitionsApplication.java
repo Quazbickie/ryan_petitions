2cce3f3c8df55c9fc06739f0a7d357687642aa39
Ryan <ryankane180@gmail.com>
2024-11-29 23:08:44 +0000

    Change RestController to Controller

--- src/main/java/com/example/demo/RyanPetitionsApplication.java
+++ src/main/java/com/example/demo/RyanPetitionsApplication.java
@@ -3,18 +3,18 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class RyanPetitionsApplication {
 
 	@Autowired
 	private PetitionService petitionService;
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
