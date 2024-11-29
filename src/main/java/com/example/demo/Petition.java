5141cd7c1520e34cc54b7fb0c2a727695f84d142
Ryan <ryankane180@gmail.com>
2024-11-29 08:34:15 +0000

    Create Petition class

---
+++ src/main/java/com/example/demo/Petition.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Petition {
+
+    private int ID;
+    private String title;
+    private String description;
+    private List<String> signatures = new ArrayList<>();
+
+    public Petition(int ID, String title, String description){
+        this.ID = ID;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getSignatures() {
+        return signatures;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+}
